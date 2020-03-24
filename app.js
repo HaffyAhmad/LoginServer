4eae4b3048c4e46741b0aa3fa1af9cdc7b969b80
Haffy Ahmad Khayam <haffy.jutt@gmail.com>
2020-03-24 15:42:29 +0500

    Add web pages

--- app.js
+++ app.js
@@ -21,19 +21,19 @@ connect.then((db)=>{
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(`${__dirname}/public`));
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 var User = require('../models/user');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan =require('morgan');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
@@ -11,6 +14,7 @@ userRouter.use(bodyParser.json());
 /* GET users listing. */
 userRouter.route('/')
 	.get(authenticate.verifyOrdinaryUser, (req,res,next) => {
+		res.render('index');
 		User.find({})
 		.then((users) => {
 			res.statusCode = 200;
@@ -20,7 +24,6 @@ userRouter.route('/')
 		 (err) => next(err))
 		.catch( (err) => next(err) );
 	});
-
 userRouter.post('/signup', (req, res, next) => {
 	User.register(new User( {username: req.body.username}), 
       req.body.password, (err,user) => {
@@ -31,17 +34,17 @@ userRouter.post('/signup', (req, res, next) => {
     	}
     	else{
       		passport.authenticate('local')(req, res, () => {
+			  res.render('signup');
     		  res.statusCode = 200;
     		  res.setHeader('Content-Type', 'application/json');
-    		  res.json({status: 'Registration Successful!', user: user});
+			  res.json({status: 'Registration Successful!', user: user});
 			});
 		}
 	  });
 });
-
-userRouter.post('/login', passport.authenticate('local'), (req, res) => {
+userRouter.post('/login',passport.authenticate('local'), (req, res) => {
+	res.render('login')
   var token = authenticate.getToken({_id: req.user._id});
-
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true,token: token,
